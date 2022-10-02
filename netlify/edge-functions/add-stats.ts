62570dd57623cf439ef8843b8afd3906d8e704b7
Chris Otto <chris.otto6@gmail.com>
2022-10-01 21:38:08 -0500

    Updating

--- netlify/edge-functions/add-stats.ts
+++ netlify/edge-functions/add-stats.ts
@@ -2,35 +2,38 @@ import type { Context } from 'https://edge.netlify.com'
 import type { Element } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts'
 import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts'
 
-export default async function (request: Request, context: Context) {
+export default async function (_request: Request, context: Context) {
   const response = await context.next()
+  console.log('EDGE BB')
 
+  return new HTMLRewriter().transform(response)
   // TODO figure out how to get URL from env
-  const stats = await fetch(
-    'http://localhost:8888/.netlify/functions/get-twitch-stats'
-  )
-  const twitch = await stats.json()
+  //const stats = await fetch(
+  //  'http://localhost:8888/.netlify/functions/get-twitch-stats'
+  //)
 
-  console.log(twitch)
-
-  const formatted = new Intl.NumberFormat('en-US', {
-    notation: 'compact',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 1,
-  }).format(twitch.stats.followers)
-  console.log(formatted)
-  return new HTMLRewriter()
-    .on('[data-site="twitch"]', {
-      element(element: Element) {
-        element.append(
-          `
-          <span class="stats">${formatted} followers</span>
-        `,
-          {
-            html: true,
-          }
-        )
-      },
-    })
-    .transform(response)
+  //const twitch = await stats.json()
+  //
+  //console.log(twitch)
+  //
+  //const formatted = new Intl.NumberFormat('en-US', {
+  //  notation: 'compact',
+  //  minimumFractionDigits: 0,
+  //  maximumFractionDigits: 1,
+  //}).format(twitch.stats.followers)
+  //console.log(formatted)
+  //return new HTMLRewriter()
+  //  .on('[data-site="twitch"]', {
+  //    element(element: Element) {
+  //      element.append(
+  //        `
+  //        <span class="stats">${formatted} followers</span>
+  //      `,
+  //        {
+  //          html: true,
+  //        }
+  //      )
+  //    },
+  //  })
+  //  .transform(response)
 }
